66b610718d34230ae43f4c436979ee125f225915
King John <jinking.this@gmail.com>
2024-10-17 15:25:19 +0800

    no lombok

--- src/main/java/com/example/demo/dameng/jdbc/config/DMConfig.java
+++ src/main/java/com/example/demo/dameng/jdbc/config/DMConfig.java
@@ -2,10 +2,9 @@ package com.example.demo.dameng.jdbc.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import lombok.Data;
 
 @Configuration
-@Data
+@SuppressWarnings("unused")
 public class DMConfig {
 
     @Value("${dmHost:\"127.0.0.1\"}")
@@ -19,4 +18,50 @@ public class DMConfig {
 
     @Value("${dmPassword:\"SYSDBA\"}")
     private String password;
+
+    public String getHost() {
+        return host;
+    }
+
+    public DMConfig setHost(String host) {
+        this.host = host;
+        return this;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public DMConfig setPort(String port) {
+        this.port = port;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public DMConfig setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public DMConfig setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "DMConfig{" +
+                "host='" + host + '\'' +
+                ", port='" + port + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
